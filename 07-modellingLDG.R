73666cf21d8b64634d3102edcf65f8630fcb6920
Jory Griffith <jory.griffith@gmail.com>
2023-05-16 14:36:51 -0400

    extracted continent from data

--- 07-modellingLDG.R
+++ 07-modellingLDG.R
@@ -6,7 +6,9 @@ library(rnaturalearth)
 library(rnaturalearthdata)
 library(countrycode)
 
-world <- ne_countries(scale = "medium", returnclass = "sf")
+world <- ne_countries(scale = "medium", type="map_units", returnclass = "sf")
+plot(world)
+?ne_countries
 st_crs(world)
 
 # load thresholded summary data
@@ -14,7 +16,6 @@ dat <- read.csv("5yr_summary/summary_thresholded.csv")
 dat <- rename(dat, lat = y, long = x)
 
 # First I need to get the other data that I want to include in the model
-
 
 ######## Assign hemisphere
 dat <- dat %>% mutate(hemisphere = if_else(lat>0, "northern", "southern"))
@@ -24,12 +25,27 @@ dat_sf <- st_as_sf(dat, coords=c('long', "lat"), crs=st_crs(world))
 
 joined <- st_join(dat_sf, world)
 
+
 dat_joined <- as.data.frame(joined[,c(1:22,41)] %>% mutate(long = sf::st_coordinates(.)[,1],
                                                            lat = sf::st_coordinates(.)[,2]))# just keep country name
 # extract continent using country name
 dat_joined$continent <- countrycode(sourcevar = dat_joined[,"name_long"],
                                      origin = "country.name",
                                      destination = "continent")
+
+
+##################
+# Trying new way to do continent
+continents <- st_read("/Volumes/Expansion/eBird/continent-poly/Continents.shp")
+plot(continents)
+
+dat_sf <- st_as_sf(dat, coords=c('long', "lat"), crs=st_crs(continents))
+
+dat_withbiome <- st_join(dat_sf, continents, left=TRUE)
+?st_join
+
+
+
 
 
 #########################
@@ -42,9 +58,13 @@ length(unique(biomes$REALM)) # 9 of these
 length(unique(biomes$BIOME)) # 16 biomes
 length(unique(biomes$ECO_NAME)) # 827 ecoregion names
 
+# plot biome
+plot(biomes["BIOME"])
+
 # want to extract biomes
-dat_sf <- st_as_sf(dat_joined, crs=st_crs(biomes))
+dat_sf <- st_as_sf(dat, crs=st_crs(biomes), coords=c('long', "lat"))
 dat_withbiome <- st_join(dat_sf, biomes[,"BIOME"])
+
 
 # create seperate columns for lat long again
 datFINAL <- dat_withbiome %>% mutate(long = sf::st_coordinates(.)[,1],
@@ -52,7 +72,29 @@ datFINAL <- dat_withbiome %>% mutate(long = sf::st_coordinates(.)[,1],
 # have a bunch of NA values for continent and biome, not sure why need to look into that
 
 # save as csv
-write.csv(datFINAL, "modeling_data.csv")
+write.csv(datFINAL, "modeling_data.csv", row.names=FALSE)
+
+
+
+
+
+
+
+
+######################################
+# Start running models
+
+dat2 <- read.csv("modeling_data.csv")
+summary(dat)
+
+
+
+
+
+
+
+
+
 
 
 

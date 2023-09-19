415c48aa1c6a03466d7b0fe8caf36cf9cb1a2ecd
Jory Griffith <jorygriffith@Sunday-Lab-iMac.local>
2023-09-19 14:31:08 -0400

    Worked on calculating the specialization indices and merging with the species level data

--- 13-specialization-analyses.R
+++ 13-specialization-analyses.R
@@ -1,28 +1,37 @@
 ########### Script for analyzing whether specialist species are being disproportionately lost at lower latitudes
 library(tidyverse)
 library(terra)
+library(taxize)
 ## First I want to thin the data so that for each cell, there is only one row for each species 
 
 years <- c(2017, 2018, 2019, 2020, 2021, 2022)
 
 names <- c("r1c1", "r1c2", "r1c3", "r1c4",
            "r2c1", "r2c2AA", "r2c2ABA", "r2c2ABB", "r2c2B", "r2c3", "r2c4",
-           "r3c1", "r3c2", "r3c3", "r3c4",
-           "r4c1", "r4c2", "r4c3", "r4c4")
+           "r3c1", "r3c2", "r3c3", "r3c4"
+          , "r4c2", "r4c4") # take out r4c1 and r4c4 because they are not in the final modelling data
+
 
 model.data <- read.csv("modeling_data.csv") 
 # also going to load this because I want to filter for cells that are in the final dataset to save space
-
+unique(model.data$square)
 GHSL <- rast("/Volumes/Backup/eBird/SMOD_global/SMOD_global.tif")
 
 datalist.years <- list()
 datalist.names <- list()
 
-for (i in 2:length(names)){
-  for (j in 1:length(years)) {
+
+for (i in 16:16){ # come back to 16, 18
+  for (j in 5:6) {
     dat <- read.table(paste("/Volumes/Backup/eBird/eBird_", years[j], "_data/custom_bbox/", names[i], "_", years[j], "_filt.txt", sep=""), 
                       header=TRUE)
     dat.filt <- dat %>% filter(cell %in% model.data$cell) # filter for cells that are in the final dataset
+    dat.filt$SCIENTIFIC.NAME<- as.character(dat.filt$SCIENTIFIC.NAME)
+    dat.filt$OBSERVATION.DATE<- as.character(dat.filt$OBSERVATION.DATE)
+    dat.filt$OBSERVER.ID<- as.character(dat.filt$OBSERVER.ID)
+    dat.filt$SAMPLING.EVENT.IDENTIFIER <- as.character(dat.filt$SAMPLING.EVENT.IDENTIFIER)
+    dat.filt$OBSERVATION.COUNT <- as.character(dat.filt$OBSERVATION.COUNT)
+    dat.filt$GROUP.IDENTIFIER <- as.character(dat.filt$GROUP.IDENTIFIER)
     datalist.years[[j]] <- dat.filt
   }
   dat2 <- dplyr::bind_rows(datalist.years) # put all years together
@@ -30,14 +39,40 @@ for (i in 2:length(names)){
     distinct(cell, SCIENTIFIC.NAME)
   dat_uniquesp$x <- xFromCell(GHSL, dat_uniquesp$cell) # extract the coordinates from the cells
   dat_uniquesp$y <- yFromCell(GHSL, dat_uniquesp$cell)
+  dat_uniquesp$square=names[i]
   datalist.names[[i]] <- dat_uniquesp
+  print(paste("finished", names[i]))
+  rm(dat)
+  rm(dat.filt)
+  rm(dat_uniquesp)
 }
 
 global_unique_sp <- dplyr::bind_rows(datalist.names) # put all sections together
-
+length(unique(global_unique_sp$SCIENTIFIC.NAME)) # 10,723 species
 write.csv(global_unique_sp, paste("global_unique_species.csv", sep=""))
 
-### Put together with 
+#### Extract urban scores
+GHSL <- rast("/Volumes/Expansion/eBird/SMOD_global/GHSL_filtMollweide.tif")
+global_uniquesp$urban <- as.data.frame(terra::extract(GHSL, global_uniquesp[,c(3:4)]))$SMOD_global
+
+### Merge with trait data
+# Use taxise to make sure taxonomy lines up
+
+diet<- read.csv("/Volumes/Backup/eBird/Traits/EltonTraits/BirdFuncDat_wgini.csv")
+
+habitat <- read.csv("/Volumes/Backup/eBird/Traits/habitat_breadth.csv")
+
+sp_diet <- merge(global_unique_sp, diet, by.x="SCIENTIFIC.NAME", by.y="Scientific")
+length(unique(sp_diet$SCIENTIFIC.NAME)) # 6,904 species
+
+sp_habitat <- merge(global_unique_sp, habitat, by.x="SCIENTIFIC.NAME", by.y="Best_guess_binomial")
+length(unique(sp_habitat$SCIENTIFIC.NAME)) # 8,498 species
+
+# Bin by latitude
+
+# See whether there are more specialist species being lost in urban areas in lower latitudes
+
+
 
 
 
